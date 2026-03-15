398ef72e746e5f0f4272d802d51a6a11791ae987
Alexey Kozovski <alexey.kozovski@idfinance.com>
2026-03-15 15:04:52 +0300

    feat: add displays welcome message

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 const main = async () => {
   // Write your code here
   //  — entry point, sets up REPL, handles navigation state
+    console.log('Welcome to Data Processing CLI!');
 };
 
 await main();
